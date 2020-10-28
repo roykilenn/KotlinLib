17b492927fbbf4b595b083ce061ab1638ded5422
KTLibTest <https://github.com/roykilenn/KTLibTest.git>
2020-10-28 08:38:58 +0800

    LibraryOnly

---
+++ testlibrary/src/main/java/com/example/testlibrary/KotlinMethod.kt
@@ -0,0 +1,16 @@
+package com.example.testlibrary
+
+public class KotlinMethod {
+    public fun Add(a:Int,b:Int): Int {
+        return a+b;
+    }
+    public fun subtract(a:Int,b:Int): Int {
+        return a-b;
+    }
+    public fun multiply(a:Int,b:Int): Int {
+        return a*b;
+    }
+    public fun divide(a:Int,b:Int): Int {
+        return a/b;
+    }
+}
